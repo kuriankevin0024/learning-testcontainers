26687b5bcb276d30ce75261f83faf5f8c55f9124
Kurian Kevin <kuriankevin0024@gmail.com>
2025-08-07 11:02:27 +0530

    Adding Milvus and Spark Iceberg QuickStart

--- README.md
+++ README.md
@@ -6,3 +6,21 @@
 
 ## delete all installed pip dependencies
 `pip freeze | grep -v " @ " | cut -d'=' -f1 | xargs pip uninstall -y`
+
+## Milvus
+Doc: https://milvus.io/docs
+
+### Run Milvus Standalone
+Docker Compose: https://milvus.io/docs/install_standalone-docker-compose.md
+Path: `milvus/docker_compose/docker-compose.yml`
+Milvus UI: http://127.0.0.1:9091/webui/
+ToDo: Develop Python Code and TestContainer Testcase
+
+## Spark Iceberg
+Doc: https://iceberg.apache.org/
+
+### Run Spark Iceberg
+Docker Compose: https://iceberg.apache.org/spark-quickstart/#docker-compose
+Path: `spark_iceberg/docker-compose.yml`
+Run Spark SQL: `spark-submit /home/iceberg/spark_iceberg.py`
+Minio UI: http://localhost:9001
---
+++ spark_iceberg/spark_iceberg.py
@@ -0,0 +1,43 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("SparkIceberg").getOrCreate()
+
+print('>>> show catalogs <<<')
+spark.sql('SHOW CATALOGS;').show()
+
+print('>>> create schema <<<')
+spark.sql("CREATE NAMESPACE IF NOT EXISTS iceberg_catalog.iceberg_schema "
+          "LOCATION 's3a://warehouse/iceberg_schema';")
+print('>>> show schemas <<<')
+spark.sql('SHOW NAMESPACES IN iceberg_catalog;').show()
+
+print('>>> create table <<<')
+spark.sql("CREATE TABLE IF NOT EXISTS iceberg_catalog.iceberg_schema.iceberg_table (id INT, name STRING, ts TIMESTAMP) "
+          "USING iceberg LOCATION 's3a://warehouse/iceberg_schema/iceberg_table';")
+print('>>> show tables <<<')
+spark.sql('SHOW TABLES IN iceberg_catalog.iceberg_schema;').show()
+
+print('>>> create row <<<')
+spark.sql("INSERT INTO iceberg_catalog.iceberg_schema.iceberg_table VALUES (1, 'john doe', current_timestamp());")
+print('>>> show rows <<<')
+spark.sql('SELECT * FROM iceberg_catalog.iceberg_schema.iceberg_table;').show()
+
+print('>>> delete row <<<')
+spark.sql("DELETE FROM iceberg_catalog.iceberg_schema.iceberg_table WHERE id IN (1);")
+print('>>> show rows <<<')
+spark.sql('SELECT * FROM iceberg_catalog.iceberg_schema.iceberg_table;').show()
+
+print('>>> drop table <<<')
+spark.sql('DROP TABLE IF EXISTS iceberg_catalog.iceberg_schema.iceberg_table;')
+print('>>> show tables <<<')
+spark.sql('SHOW TABLES IN iceberg_catalog.iceberg_schema;').show()
+
+print('>>> drop schema <<<')
+spark.sql('DROP NAMESPACE IF EXISTS iceberg_catalog.iceberg_schema CASCADE;')
+print('>>> show schemas <<<')
+spark.sql('SHOW NAMESPACES IN iceberg_catalog;').show()
+
+print('>>> show catalogs <<<')
+spark.sql('SHOW CATALOGS;').show()
+
+spark.stop()
